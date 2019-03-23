9d2e1ad3437fbbf8754018ce92ef7f00cfa793ca
rajan <rajangreedge@gmail.com>
2019-03-23 22:53:52 +0530

    created basic PostList component

---
+++ src/components/PostList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Component} from 'react';
+
+class PostList extends Component {
+
+    render() {
+        return (<div>PostList</div>)
+    }
+}
+export default PostList;
