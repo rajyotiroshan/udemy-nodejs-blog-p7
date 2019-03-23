0ffea89f3c1f3b0b695a8914c5c1f31c31023fb5
rajan <rajangreedge@gmail.com>
2019-03-23 23:18:00 +0530

    calling action creator from cdm

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux';
 import {Component} from 'react';
+import {fetchPost} from '../actions';
 
 class PostList extends Component {
+componentDidMount(){
+    this.props.fetchPost();
+}
 
     render() {
         return (<div>PostList</div>)
     }
 }
-export default PostList;
+export default connect(null,{fetchPost})(PostList);
