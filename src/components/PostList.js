c6de432a9effb2375e4296f970e7d01d3e2d10e4
rajan <rajangreedge@gmail.com>
2019-04-11 10:43:50 +0530

    connect to redux store

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,8 +8,32 @@ class PostList extends Component {
         this.props.fetchPost();
     }
 
-    render() {
-        return (<div>PostList</div>)
+    renderList() {
+        console.log(this.props.posts);
+        return this.props.posts.map(post => {
+            return (
+                <div className='item' key={post.id}>
+                    <i className='large middle aligned icon user'/>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render() { 
+        return (<div className='ui relaxed divided list'>{this.renderList()}
+        </div>)
     }
 }
-export default connect(null,{fetchPost})(PostList);
+
+const mapStateToProps = state => {
+    return {posts: state.posts};
+}
+export default connect(
+    mapStateToProps,
+    {fetchPost})(PostList);
